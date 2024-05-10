09c276cfcb9d28cdf3283e68f74e585d688dd509
hjlee <hjlee@3top.co.kr>
2024-05-10 09:13:26 +0900

    feat: jsonDiff 스웨거 어노테이션을 활용하여 화면용 path 번역 기능 추가

--- src/main/java/com/demo1/jsondiff/HistoryUtil.java
+++ src/main/java/com/demo1/jsondiff/HistoryUtil.java
@@ -46,58 +46,63 @@ public class HistoryUtil {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode diffNode = JsonDiff.asJson(mapper.valueToTree(original), mapper.valueToTree(change));
 
+        if (diffNode.isEmpty()) {
+            // ToDo 널 return 시, 받는 쪽에서 분기 필요
+            return null;
+        } else {
 
-        JsonNode diffArray = mapper.readTree(diffNode.toPrettyString());
-        Class<?> clazz = original.getClass();
+            JsonNode diffArray = mapper.readTree(diffNode.toPrettyString());
+            Class<?> clazz = original.getClass();
 
-        //reflection field 돌면서 key(field) : value(annotation) 담기
-        Map<String, String > fieldMap = new HashMap<>();
-        for(Field field : clazz.getDeclaredFields()) {
-            ApiModelProperty apiModelProperty = field.getAnnotation(ApiModelProperty.class);
-            fieldMap.put(field.getName(), apiModelProperty.value());
-        }
-
-
-        List<DiffDto> jsondiffDtoList = new ArrayList<>();
-
-        for (JsonNode node : diffArray) {
-            DiffDto dto = new DiffDto();
-            dto.setOp(Category.fromValue(node.get("op").asText()));
-
-            //ToDo : path 구분로직 재검토 필요
-            String [] processedPaths = node.get("path").asText().split("/");
-            String processedPath = processedPaths[1];
-            dto.setPath(fieldMap.get(processedPath));
-
-
-            JsonNode beforeNode = node.get("before");
-            if (beforeNode != null) {
-                if (beforeNode.isObject()) {
-                    dto.setBefore(beforeNode.toString());
-                } else {
-                    dto.setBefore(beforeNode.asText());
-                }
+            //reflection field 돌면서 key(field) : value(annotation) 담기
+            Map<String, String> fieldMap = new HashMap<>();
+            for (Field field : clazz.getDeclaredFields()) {
+                ApiModelProperty apiModelProperty = field.getAnnotation(ApiModelProperty.class);
+                fieldMap.put(field.getName(), apiModelProperty.value());
             }
 
-            JsonNode afterNode = node.get("after");
-            if (afterNode != null) {
-                if (afterNode.isObject()) {
-                    dto.setAfter(afterNode.toString());
 
-                } else {
-                    dto.setAfter(afterNode.asText());
+            List<DiffDto> jsondiffDtoList = new ArrayList<>();
+
+            for (JsonNode node : diffArray) {
+                DiffDto dto = new DiffDto();
+                dto.setOp(Category.fromValue(node.get("op").asText()));
+
+                //ToDo : path 구분로직 재검토 필요
+                String[] processedPaths = node.get("path").asText().split("/");
+                String processedPath = processedPaths[1];
+                dto.setPath(fieldMap.get(processedPath));
+
+
+                JsonNode beforeNode = node.get("before");
+                if (beforeNode != null) {
+                    if (beforeNode.isObject()) {
+                        dto.setBefore(beforeNode.toString());
+                    } else {
+                        dto.setBefore(beforeNode.asText());
+                    }
                 }
-            }
-            jsondiffDtoList.add(dto);
-        }
-        return jsondiffDtoList; // DB에 넣기
 
-        /* predict
-         * 분류 |  필드명  |    변경 전  |   변경 후
-         * ========================================
-         * 수정 | 이메일   |  apple@com | banana@com
-         * -----------------------------------------
-        */
-    }
+                JsonNode afterNode = node.get("after");
+                if (afterNode != null) {
+                    if (afterNode.isObject()) {
+                        dto.setAfter(afterNode.toString());
+
+                    } else {
+                        dto.setAfter(afterNode.asText());
+                    }
+                }
+                jsondiffDtoList.add(dto);
+            }
+            return jsondiffDtoList; // DB에 넣기
+
+            /* predict
+             * 분류 |  필드명  |    변경 전  |   변경 후
+             * ========================================
+             * 수정 | 이메일   |  apple@com | banana@com
+             * -----------------------------------------
+             */
+        }
+     }
 }
 
